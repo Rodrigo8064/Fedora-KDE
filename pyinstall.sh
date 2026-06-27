aed7934d443457d5fa1abf23dc8bd5c66b31bf67
Rodrigo Lima <rodrigooliveiradel@gmail.com>
2026-06-26 22:34:24 -0300

    Add system update commands to pyinstall.sh
    Added a section to update system packages using dnf.

---
+++ pyinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# python
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+source ~/.zshrc
+pyenv install 3.13.13
+pyenv global 3.13.13
+python --version
+pip install --upgrade pip
+pip install virtualenv pipx isort mypy ipython
+pipx ensurepath
+pipx install poetry
+pipx upgrade-all
+poetry config virtualenvs.in-project true
+
+# zuban
+pip install zuban
+
+# Atualizar
+sudo dnf update -y && sudo dnf upgrade -y

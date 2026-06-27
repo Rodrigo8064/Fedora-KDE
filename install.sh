236285abc6ac334f900966c2293f3702c0f5119b
Rodrigo Lima <rodrigooliveiradel@gmail.com>
2026-06-26 22:32:29 -0300

    Remove Python setup instructions from install.sh
    Removed Python installation and related configurations from the script.

--- install.sh
+++ install.sh
@@ -35,26 +35,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # Edite ~/.zshrc e mude: ZSH_THEME="powerlevel10k/powerlevel10k"
 # Reinicie o terminal e siga o assistente de configuração
 
-# python
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-source ~/.zshrc
-pyenv install 3.13.13
-pyenv global 3.13.13
-python --version
-pip install --upgrade pip
-pip install virtualenv pipx isort mypy ipython
-pipx ensurepath
-pipx install poetry
-pipx upgrade-all
-poetry config virtualenvs.in-project true
-
-# zuban
-pip install zuban
-
-
 # Utilitarios extras
 sudo dnf install -y htop neofetch tree unzip p7zip
 
